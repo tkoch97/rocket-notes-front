dd9088d2503e9aba3954204dcd77270a87c941fe
Thiago Koch <tkoch.dev@gmail.com>
2023-06-22 15:49:19 -0300

    feat: creat page SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,29 +1,30 @@
 import { Container, Form, BackgroundImg } from './styles';
 import { Input } from '../../components/input';
 import { Button } from '../../components/button';
-import { FiMail, FiLock } from 'react-icons/fi';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
 
-export function SignIn() {
+export function SignUp() {
   return(
     <Container>
+      <BackgroundImg 
+        alt='Imagem de uma mesa com um notebook, 
+        um bloco de notas, uma 
+        caneta e uma caneca com café'
+      />
+
       <Form>
         <h1>RocketNotes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
 
+        <Input placeholder='Nome' type="text" icon={FiUser}/>
         <Input placeholder='E-mail' type="text" icon={FiMail} />
         <Input placeholder='Senha' type="password" icon={FiLock}/>
 
-        <Button title='Entrar'/>
+        <Button title='Cadastrar'/>
 
-        <a href="">Criar conta</a>
+        <a href="">Voltar para o login</a>
       </Form>
-
-      <BackgroundImg 
-        alt='Imagem de uma mesa com um notebook, 
-        um bloco de notas, 
-        uma caneta e uma caneca com café '
-      />
     </Container>
-  );
+  )
 }
