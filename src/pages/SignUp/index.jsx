51b6ffdaab777845a2bd31eb9d1ea7b0dd47303e
Thiago Koch <tkoch.dev@gmail.com>
2023-07-18 18:38:06 -0300

    feat: start integration with backend by axios and add AuthContext

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,10 +1,38 @@
 import { Container, Form, BackgroundImg } from './styles';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { api } from "../../services/api"
 import { Input } from '../../components/input';
 import { Button } from '../../components/button';
-import { FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
 
 export function SignUp() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if( !name || !email || !password ) {
+      return alert("Preencha todos os campos");
+    }
+
+    api.post("/users", {name, email, password})
+    .then(() => {
+      alert("Usuário cadastrado com sucesso!");
+      navigate('/');
+    })
+    .catch(error => {
+      if(error.response) {
+        alert(error.response.data.message);
+      }else{
+        alert("Não foi possível efetuar o cadastro.");
+      }
+    })
+  }
+
+
   return(
     <Container>
       <BackgroundImg 
@@ -18,11 +46,22 @@ export function SignUp() {
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
         <h2>Crie sua conta</h2>
 
-        <Input placeholder='Nome' type="text" icon={FiUser}/>
-        <Input placeholder='E-mail' type="text" icon={FiMail} />
-        <Input placeholder='Senha' type="password" icon={FiLock}/>
+        <Input placeholder='Nome'
+        type="text" 
+        icon={FiUser}
+        onChange={event => setName(event.target.value)}/>
 
-        <Button title='Cadastrar'/>
+        <Input placeholder='E-mail' 
+        type="text" 
+        icon={FiMail}
+        onChange={event => setEmail(event.target.value)} />
+
+        <Input placeholder='Senha' 
+        type="password" 
+        icon={FiLock}
+        onChange={event => setPassword(event.target.value)} />
+
+        <Button title='Cadastrar' onClick={ handleSignUp }/>
 
         <Link to="/">Voltar para o login</Link>
       </Form>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { AppRoutes } from "./app.routes";
+// import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
